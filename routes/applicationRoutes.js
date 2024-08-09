c6f4872344cf416b1e6eed00c71f043e3807c608
Puru <purugcit79@gmail.com>
2024-08-09 19:21:47 +0000

    routes for application

--- routes/applicationRoutes.js
+++ routes/applicationRoutes.js
@@ -6,5 +6,14 @@ const router = express.Router()
 router
     .route('/')
     .post(authController.protect, applicationController.uploadVerificationDocument, applicationController.createApplication)
+    .get(authController.protect, applicationController.getAllApplications) 
+
+router
+    .route('/:id')
+    .get(authController.protect, applicationController.getApplication) 
+    .patch(authController.protect, applicationController.uploadVerificationDocument, applicationController.updateApplication) 
+    .delete(authController.protect, applicationController.deleteApplication); 
+
+module.exports = router;
 
 module.exports = router
